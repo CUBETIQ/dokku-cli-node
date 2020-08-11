7c00598c715fc8fb542ac80b1d01c89be34e91f1
Sambo Chea <sombochea@cubetiqs.com>
2020-08-11 20:30:44 +0700

    Add libs for welcome text

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 
 const { exec } = require("child_process");
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const greeting = chalk.white.bold("Welcome to CUBETIQ App Deployment!");
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+};
+const msgBox = boxen(greeting, boxenOptions);
+
+console.log(msgBox);
 
 exec("ssh -t dokku@osa.cubetiqs.com ps:report", (err, stdout, stderr) => {
   if (err) {
