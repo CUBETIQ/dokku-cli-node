ad61ef32ff88638ec22d21a882a7d8c662b058e8
Sambo Chea <sombochea@cubetiqs.com>
2020-08-11 21:00:04 +0700

    Updated the commands list

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,9 @@ const chalk = require("chalk");
 const boxen = require("boxen");
 const yargs = require("yargs");
 
-const greeting = chalk.white.bold("Welcome to CUBETIQ App Deployment!");
+const greeting = chalk
+  .hex("#0fcbff")
+  .bold("Welcome to CUBETIQ App Deployment!");
 
 const boxenOptions = {
   padding: 1,
@@ -14,18 +16,24 @@ const boxenOptions = {
   borderColor: "green",
   backgroundColor: "#555555",
 };
-const msgBox = boxen(greeting, boxenOptions);
 
-console.log(msgBox);
+console.log(boxen(greeting, boxenOptions));
 
-const options = yargs.usage("Usage: dokku option").option("cmd", {
-  alias: "command",
-  describe: "Dokku command",
-  type: "string",
-  demandOption: false,
-}).argv;
+const options = yargs
+  .usage("Usage: cubetiq-app dokku")
+  .option("dokku", {
+    describe: "Dokku command",
+    type: "string",
+    demandOption: false,
+  })
+  .option("dokku help", {
+    describe: "Dokku help command",
+    type: "string",
+    demandOption: false,
+  }).argv;
 
-const cmd = options.cmd;
+const cmd = options.dokku;
+const help = options["dokku help"];
 
 if (cmd) {
   exec(`ssh -t dokku@osa.cubetiqs.com ${cmd}`, (err, stdout, stderr) => {
@@ -36,12 +44,17 @@ if (cmd) {
     console.log(stdout);
   });
 } else {
-  exec(`ssh -t dokku@osa.cubetiqs.com`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
+  if (help) {
+    exec(`ssh -t dokku@osa.cubetiqs.com`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
+  } else {
+    const errMsg = chalk.red("no commd found!");
+    console.error(errMsg);
+  }
   return;
 }
